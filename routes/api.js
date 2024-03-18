7f3e352a7869c4c802b8f12310b92a4a12fbe0ea
12sachin95 <12sachin95@gmail.com>
2024-03-18 19:09:38 +0530

    FEAT: cors, helmet, redis-cache and rate limiter functionality added

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ import AuthController from "../controllers/AuthController.js";
 import authMiddleware from "../middleware/Authenticate.js";
 import ProfileController from "../controllers/ProfileController.js";
 import NewsController from "../controllers/NewsController.js";
+import redisCache from "../DB/redis-config.js";
 
 const router = Router();
 
@@ -12,9 +13,9 @@ router.post("/auth/login", AuthController.login);
 router.get("/profile", authMiddleware, ProfileController.getUserProfile);
 router.put("/profile/:id", authMiddleware, ProfileController.updateUserProfile);
 //news apis
-router.get("/news/", authMiddleware, NewsController.getAllNews);
+router.get("/news/", redisCache.route(), NewsController.getAllNews);
 router.post("/news/", authMiddleware, NewsController.createNews);
-router.get("/news/:id", authMiddleware, NewsController.getNews);
+router.get("/news/:id", redisCache.route(), NewsController.getNews);
 router.put("/news/:id", authMiddleware, NewsController.updateNews);
 router.delete("/news/:id", authMiddleware, NewsController.deleteNews);
 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import "dotenv/config";
 import ApiRoutes from "./routes/api.js";
 import expressFileUpload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
+import { limiter } from "./config/ratelimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,6 +14,11 @@ app.use(express.json()); //parse request body as JSON()
 app.use(express.urlencoded({ extended: false })); //Parse URL
 app.use(expressFileUpload()); //for file uploading
 app.use(express.static("public")); //Serve static files (css  , js)
+app.use(helmet());
+app.use(cors());
+app.use(limiter);
+
+//API routes
 
 app.get("/", (req, res) => {
   return res.json({ message: "hello" });
